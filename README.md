6f0373e7f6a86fea6efc2838c9ff4a582a164df3
Mac User <laser_yellow@cnc.jp>
2017-10-14 20:24:00 +0900

    node サーバー用に編集

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # a-frame-video-server
+
+## 環境
+
+|名称|バージョン|備考|
+|---|---|---|
+|node|v8.7.0|実行環境|
+|yarn|1.2.1|パッケージマネージャー|
+|yargs|9.0.1|node fuse に引数を渡す|
+|fuse-box|2.3.3|モジュールバンドラー|
+|typescript|2.5.3|Compiler|
+
+## 構築
+
+### yarn のインストール
+homebrew で yarn をインストール
+
+```$ brew install yarn```
+
+### モジュールをインストール
+
+```
+$ cd a-frame-video-server
+$ yarn install # yarn.lock を使用してインストール
+```
+
+## ビルド
+
+```
+$ npm run build
+```
--- fuse.js
+++ fuse.js
@@ -1,26 +1,12 @@
 const { FuseBox } = require('fuse-box');
 const argv = require('yargs').argv;
 
-const dev = argv.variant === 'dev';
-
 const fuse = FuseBox.init({
     homeDir: 'src/scripts',
     output: 'public/build/$name.js',
-    sourceMaps: dev,
-    cache: dev
 });
-
-if (dev) {
-    fuse.dev({
-        port: 8080, // port 番号を指定。デフォルトは 4444
-        open: true, // ブラウザで表示
-        httpServer: true, // http サーバ機能を有効化するかどうかを指定。デフォルトは true
-    });
-}
 
 const app = fuse.bundle('app')
     .instructions(`> main.ts`);
-
-dev && app.watch().hmr();
 
 fuse.run();
